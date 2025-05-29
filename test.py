f991c07cdd029b6943a4e7a71d0a06a52bf2e4d9
Yanvansnip <87695213+vanSnip@users.noreply.github.com>
2025-05-29 21:45:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,5 @@ import streamlit.components.v1 as components
 # Your full HTML content as a string (paste your whole HTML here)
 with open("test.html", "r", encoding="utf-8") as f:
     html_code = f.read()
-
-# Render your HTML + JS in Streamlit using components.html
+    
 components.html(html_code, height=700, scrolling=True)

bfbec4a1ba247bd1b4f6ffdc36ed96fd562322c0
Yanvansnip <87695213+vanSnip@users.noreply.github.com>
2025-07-02 00:01:27 +0200

    Delete old_files directory

--- old_files/import_html.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-# Your full HTML content as a string (paste your whole HTML here)
-with open("test.html", "r", encoding="utf-8") as f:
-    html_code = f.read()
-    
-components.html(html_code, height=700, scrolling=True)

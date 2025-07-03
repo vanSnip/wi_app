fd97c28531d0691c96bcb9710bb16942e12c7827
yan <yanvansnippenburg@gmail.com>
2025-07-03 02:40:01 +0200

    only show warning if data saving

--- test.py
+++ test.py
@@ -157,7 +157,9 @@ def reset():
 # --- Screens ---
 def render_welcome(_=None):
     st.header("Xin Chao, what would you like to know? ")
-    st.info("The standard Versions is the data saving version, if you want acces to pictures, please change the setting with the button \"Version and Locations\" " )
+    version = st.session_state
+    if version == "data_saving":
+        st.info("The current versions is the data saving version, if you want acces to pictures, please change the setting with the button \"Version and Locations\" " )
     st.button("Weather information", on_click=partial(navigate, "weather_info"))
     st.button("Crop Advice", on_click=partial(navigate, "crop_advice_1"))
     st.button("Crop Prices", on_click=partial(navigate, "price_info_1"))

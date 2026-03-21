4ebb7f46cc17b9d94572bf122f10493c01c8a6fa
Yan van Snippenburg <87695213+vanSnip@users.noreply.github.com>
2026-03-21 15:48:13 +0100

    Update GitHub username and repository name

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import pandas as pd
 import requests
 import ast
 
-user = "vanSnip"  # GitHub username
-repo = "wi_app"  # GitHub repository name
+user = "Name"  # GitHub username
+repo = "Repo_Name"  # GitHub repository name
 repo_url = f"https://raw.githubusercontent.com/{user}/{repo}/main"
 
 #-- import data --
